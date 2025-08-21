c939a4076cae9c60890c294c6e5888441d0596c7
SparkyGuy <tur.roberto456@gmail.com>
2025-08-20 23:39:43 -0300

    fix: fix some visual bugs

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,31 +1,57 @@
 import Coffe from '../assets/images/coffe.png'
 import InfoCards from './infocards';
-import { MoveLeft, Coffee } from 'lucide-react';
-function Hero (){
-    return(
-        <div className="bg-[#E5CFB5] flex flex-col md:flex-row  justify-around ">
-            <div className="flex flex-col md:p-0 pt-10 justify-center">
-                <h1 className="text-[#4A3C31] font-semibold md:text-6xl text-5xl text-center font-[lexend] md:text-left">A cozy place to enjoy <br /> your coffee</h1>
-                <h1 className='mt-4 font-[inter] font-light md:text-left text-center'>Freshly brewed coffee and delicious treats, crafted with passion to <br /> brighten your day. Relax, sip, and savor the moments with us.</h1>
-                <div className=' justify-center md:justify-start flex flex-row gap-6'>
-                    <button className="flex  bg-[#4A3C31] text-white py-4 px-4 rounded-full mt-4 transition hover:scale-110 hover:cursor-pointer">View Menu
-                        <MoveLeft size={20} className='ml-2  -scale-x-100'/>
+import { MoveLeft, Coffee, Heart } from 'lucide-react';
+
+function Hero() {
+    return (
+        <div className="relative bg-[#E5CFB5] flex flex-col md:flex-row justify-around md:gap-8 gap-10 overflow-hidden">
+
+            <div className="flex flex-col md:p-0 pt-4 justify-center">
+
+                <h1 className="text-[#4A3C31] font-semibold md:text-6xl text-5xl text-center font-[lexend] md:text-left">
+                    A cozy place to enjoy <br /> your coffee
+                </h1>
+
+                <h1 className='mt-4 font-[inter] font-light md:text-left text-center'>
+                    Freshly brewed coffee and delicious treats, crafted with passion to <br /> brighten your day. Relax, sip, and savor the moments with us.
+                </h1>
+
+                <div className="md:hidden flex justify-center mt-6">
+                    <img className="w-60 md:w-52" src={Coffe} alt="Coffe" />
+                </div>
+
+                <div className='justify-center md:justify-start flex flex-row gap-6 mt-4'>
+                    <button className="flex bg-[#4A3C31] text-white py-4 px-4 rounded-full transition hover:scale-110 hover:cursor-pointer">
+                        View Menu
+                        <MoveLeft size={28} className='ml-2 -scale-x-100' />
                     </button>
-                    <button className="flex  hover:bg-[#4A3C31] hover:text-white hover:cursor-pointer transition bg-transparent border-2 border-[#4A3C31] text-[#4A3C31] py-4 px-6  hover:scale-110 rounded-full mt-4">Visit Us</button>
+                    <button className="flex hover:bg-[#4A3C31] hover:text-white hover:cursor-pointer transition bg-transparent border-2 border-[#4A3C31] text-[#4A3C31] py-4 px-6 hover:scale-110 rounded-full">
+                        Visit Us
+                    </button>
+                </div>
+                <div className="absolute w-96 left-44 md:hidden  gap-4 mt-20 items-center ">
+                    <InfoCards icon={<Heart size={20} />} title="Trusted by 10,000++" subtitle="Coffe Lovers!" />
+                </div>
+                <div className="absolute w-96 left-8 bottom-40 md:hidden  gap-4 mt-6 items-center ">
+                    <InfoCards icon={<Coffee size={20} className="text-[#4A3C31]" />} title="50+ Menu" subtitle="Choices available" />
+                </div>
+
+            </div>
+
+            <div className="hidden md:flex relative justify-center overflow-hidden">
+                <img className="w-2xl" src={Coffe} alt="Coffe" />
+
+                <div className='absolute top-[24rem] left-0 w-96'>
+                    <InfoCards icon={<Coffee size={40} className="text-[#4A3C31]" />} title="50+ Menu" subtitle="Choices available" />
+                </div>
+
+                <div className='absolute top-10 left-[18rem] w-96'>
+                    <InfoCards icon={<Heart size={40} />} title="Trusted by 10,000++" subtitle="Coffe Lovers!" />
                 </div>
             </div>
-            <div className="relative flex justify-center ">
-                <img className="w-64 md:w-2xl" src={Coffe} alt="Coffe" />
-            </div>
-            <div className="absolute w-full bottom-10 left-5 md:top-135 md:left-240">
-                <InfoCards icon={<Coffee size={35} />} title="50+ Menu" userWrapper subtitle="Choices available" />
-            </div>
-            <div className="absolute w-full bottom-10 left-50  md:top-40 md:left-340">
-                <InfoCards icon="" title="Trusted by 10,000++" subtitle="Coffee Lovers!" />
-            </div>
+
         </div>
     )
 }
 
-
-export default Hero
+export default Hero;
--- src/components/infocards.tsx
+++ src/components/infocards.tsx
@@ -4,28 +4,22 @@ import type { ReactNode } from "react";
         icon?: ReactNode;
         title: string;
         subtitle: string;
-        userWrapper?: boolean;
     }
-function InfoCards ({icon, title, subtitle, userWrapper}: CardProps) {
+function InfoCards ({icon, title, subtitle, }: CardProps) {
 
-    const content = (
-    <div className="relative">
-        <div className="md:pr-10 absolute flex items-center gap-2 md:gap-5 bg-white rounded-3xl shadow-md p-3 md:p-4">
+
+    return (
+        <div className="md:pr-10 absolute flex items-center gap-1 md:gap-5 bg-white rounded-xl md:rounded-3xl shadow-md p-1 md:p-4">
       <div className="text-2xl">{icon}</div>
       <div>
         <h3 className="md:text-lg text-[10px] font-bold">{title}</h3>
         <p className="md:text-sm text-[8px] text-gray-500">{subtitle}</p>
       </div>
     </div>
-    </div>
 
     );
 
-  return userWrapper ? (
-    <div><h1></h1>{content}</div>
-  ) : (
-    content
-  );
+
 
 }
 export default InfoCards;
