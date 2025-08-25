337a657440b9103b6ff0b24f49c00efb7619224d
arthurnsm <tur.roberto456@gmail.com>
2025-08-24 22:38:02 -0300

    feat: add Signature section

--- src/components/signatures.tsx
+++ src/components/signatures.tsx
@@ -6,7 +6,7 @@ import Espresso from '../assets/images/espresso.png'
 
 function Signature(){
     return(
-        <div className="md:p-20 mt-12 md:mt-10 flex flex-col align-center justify-center bg-[#ffffff]">
+        <section className="md:p-20 mt-12 md:mt-10 flex flex-col align-center justify-center bg-[#ffffff]">
             <h1 className=" text-center text-4xl font-bold text-[#3E2723] font-[lexend]">Explore Our Signature Blends</h1>
             <h1 className="text-center p-6 md:p-4 font-[lexend] font-light text-[#626262]">Crafted with care, our coffee selections offer rich flavors and aromas to delight every<br/> coffee enthusiast. Experience the perfect brew in every sip</h1>
             <div className='mt-10 flex flex-col md:flex-row justify-center gap-12'>
@@ -15,7 +15,7 @@ function Signature(){
                 <Card title="Espresso" image={Espresso} price="$2.50"/>
                 <Card title="Coffe Milk" image={CoffeMilk} price="$3.50"/>
             </div>
-        </div>
+        </section>
     )
 
 }
