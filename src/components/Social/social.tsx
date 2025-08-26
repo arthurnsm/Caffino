e95e950f4ac58382b4384cfedada962c863e3488
arthurnsm <tur.roberto456@gmail.com>
2025-08-26 18:23:45 -0300

    add social component

--- src/components/Social/social.tsx
+++ src/components/Social/social.tsx
@@ -1,10 +1,25 @@
+import Image1 from '../../assets/images/img-1.png'
+import Image2 from '../../assets/images/img-2.png'
+import Image3 from '../../assets/images/img-3.png'
+import Image4 from '../../assets/images/img-4.png'
+
 function Social(){
     return(
         <section className="flex justify-center">
-            <div className="flex flex-row gap-2">
-                <h1 className="font-[lexend] text-[#3E2723] font-bold text-3xl">Follow us on</h1>
-                <h1 className="font-[lexend] text-[#E5CFB5] font-bold text-3xl">lnstagram</h1>
+            <div className="flex flex-col">
+                <h1 className="font-[lexend] text-center text-[#3E2723] font-bold text-3xl">Follow us on Instagram</h1>
+                <h1 className="font-[lexend] mt-4 text-center font-light text-xl text-[#626262]">Stay connected with us for the latest updates, promotions, and a behind-<br/>the-scenes look at our coffee creations.</h1>
+                <div className='flex flex-row w-full mt-12'>
+                    <img className='w-200' src={Image1}/>
+                    <img className='w-200' src={Image2}/>
+                    <img className='w-200' src={Image3}/>
+                    <img className='w-200' src={Image4}/>
+                </div>
+                <div className='mt-12 flex justify-center'>    
+                    <button className='bg-[#3E2723] rounded-full font-[lexend] text-white h-18 w-40'> Follow Us</button>
+                </div>
             </div>
+
         </section>
     )
 
