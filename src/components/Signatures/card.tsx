6e646465244529978ff8e816a596c38d854db12c
arthurnsm <tur.roberto456@gmail.com>
2025-08-25 21:05:14 -0300

    style: add some transitions

--- src/components/Signatures/card.tsx
+++ src/components/Signatures/card.tsx
@@ -9,7 +9,7 @@ interface  BlendProps{
 function Card({image, title, price }:BlendProps){
 
     return(
-        <div className='flex flex-row align-center justify-center'>
+        <div className='flex hover:scale-105 transition flex-row align-center justify-center'>
             <div className=' border-[#B0B0B0] p-6 rounded-2xl border-2'>
             <img className="w-56" src={image} />
             <div className='flex flex-row justify-between'>
@@ -20,7 +20,7 @@ function Card({image, title, price }:BlendProps){
                 <h1 className='font-[lexend] mt-3 text-xl text-[#898989] font-light'>{price}</h1>
                 </div>
             </div>
-            <div className='p-3 gap-3 border-[#3E2723] justify-center align-center items-center flex flex-row mt-6 rounded-3xl border'>
+            <div className='p-3 gap-3 border-[#3E2723] transition justify-center align-center items-center flex flex-row mt-6 rounded-3xl border'>
                 <ShoppingCart size={20} />
                 <h1 className='text-center'>Add Chart</h1>
             </div>
